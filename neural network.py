710ea365dc65c47c8e0bcdd51e6fcb444f294409
Philippe <109043448+philippekarst@users.noreply.github.com>
2022-10-09 22:28:59 +0200

    stochastic gradient descent

--- neural network.py
+++ neural network.py
@@ -5,23 +5,25 @@ import pandas as pd
 #Load the data
 df_train = pd.read_csv(r"C:\Users\Philippe\Documents\python codes\neural network hand written digits\mnist_train.csv")
 df_test = pd.read_csv(r"C:\Users\Philippe\Documents\python codes\neural network hand written digits\mnist_test.csv")
-df_train = pd.DataFrame(df_train).to_numpy().transpose()
+df_train = pd.DataFrame(df_train).to_numpy().transpose() #we want every column to represent a picture together with its label
 df_test = pd.DataFrame(df_test).to_numpy().transpose()
-print(f"The training data is a 2 dimensional array of following shape: {df_train.shape}")
-print(f"The testing data is a 2 dimensional array of following shape: {df_test.shape}")
-X_test = df_test[1:,:]
+print(f"Training data shape: {df_train.shape}")
+print(f"Testing data shape: {df_test.shape}")
+
+
+#preprocess the data
+df_train = df_train[:, np.random.permutation(df_train.shape[1])] #shuffle columns of the training data
 X_train = df_train[1:,:]/255
-Y_test = df_test[0,:]
 Y_train = df_train[0,:]
+print(X_train.shape)
 
-x = np.array([[1,1,2],[1,1,1]])+np.array([[-1],[3]])
-y = np.array([2,1,2])
-#W_1 = np.random.randn(10,784)
-#c=np.dot(W_1,X_train)
-#print(c.shape)
-Y=np.array([0,2,1,3,3])
-
-#print(type(Y[1]))
+X_trainsamples = []
+Y_trainsamples = []
+for i in range(60):
+    X_trainsamples.append(X_train[:,1000*i:1000*i+1000])
+    Y_trainsamples.append(Y_train[1000*i:1000*i+1000])
+X_test = df_test[1:,:]
+Y_test = df_test[0,:]
 
 
 #initialize the weights ans biases of the network
@@ -61,14 +63,14 @@ def one_hot(Y):
 #back propagation
 def back_prop(Z_1, Z_2, A_1, A_2, W_2, Y, X):
     Y = one_hot(Y)
-    dW_2 = 1/59999*2*np.dot((A_2-Y),(sigmoid(Z_2)*(1-sigmoid(Z_2))*A_1).transpose())
-    predB_2 = 1/59999*np.diag(np.dot(sigmoid(Z_2)*(1-sigmoid(Z_2)),(2*(A_2-Y)).transpose()))
+    dW_2 = 1/(X.shape[1])*2*np.dot((A_2-Y),(sigmoid(Z_2)*(1-sigmoid(Z_2))*A_1).transpose())
+    predB_2 = 1/(X.shape[1])*np.diag(np.dot(sigmoid(Z_2)*(1-sigmoid(Z_2)),(2*(A_2-Y)).transpose()))
     dB_2 = np.zeros([10,1])
     for i in range(10):
         dB_2[i,0] = predB_2[i]
     dA_1 = 2*np.dot((A_2-Y).transpose(),W_2).transpose()
-    dW_1 = 1/59999*np.dot(dA_1*(sigmoid(Z_1)*(1-sigmoid(Z_1))),X.transpose())
-    predB_1 = 1/59999*np.diag(np.dot(sigmoid(Z_1)*(1-sigmoid(Z_1)),dA_1.transpose()))
+    dW_1 = 1/(X.shape[1])*np.dot(dA_1*(sigmoid(Z_1)*(1-sigmoid(Z_1))),X.transpose())
+    predB_1 = 1/(X.shape[1])*np.diag(np.dot(sigmoid(Z_1)*(1-sigmoid(Z_1)),dA_1.transpose()))
     dB_1 = np.zeros([10,1])
     for i in range(10):
         dB_1[i,0] = predB_1[i]
@@ -83,9 +85,11 @@ def update_params(W_1, B_1, W_2, B_2, dW_1, dB_1, dW_2, dB_2, alpha):
     B_2 = B_2-alpha*dB_2
     return W_1, W_2, B_1, B_2
 
+
 #get the prediction
 def get_predictions(A_2):
     return np.argmax(A_2,0)
+
 
 #get accuracy
 def get_accuracy(predictions, Y):
@@ -96,16 +100,28 @@ def get_accuracy(predictions, Y):
             m=m+1
     return m/(T)
 
+
 #gradient descent
-def gradient_descent(X, Y, iterations, alpha):
-    W_1, W_2, B_1, B_2 = init()
+def gradient_descent(X, Y, iterations, alpha, W_1, W_2, B_1, B_2):
     for i in range(iterations):
         Z_1, Z_2, A_1, A_2 = forward_prop(W_1, W_2, B_1, B_2, X)
         dW_1, dB_1, dW_2, dB_2 = back_prop(Z_1, Z_2, A_1, A_2, W_2, Y, X)
         W_1, W_2, B_1, B_2 = update_params(W_1, B_1, W_2, B_2, dW_1, dB_1, dW_2, dB_2, alpha)
-        if i % 10 == 0:
-            a = get_accuracy(get_predictions(A_2),Y)
-            print(f"Iterations: {i}. Accuracy: {a*100}%.")
-    return W_1, W_2, B_1, B_2
-gradient_descent(X_train, Y_train, 100000000000, 1)
+        #if i % 10 == 0:
+        #    a = get_accuracy(get_predictions(A_2),Y)
+        #    print(f"Iterations: {i}. Accuracy: {a*100}%.")
+    return W_1, W_2, B_1, B_2, A_2
 
+
+#stochastic gradient descent
+def stochastic_gradient_descent(X_trainsamples, Y_trainsamples, iterations, alpha):
+    W_1, W_2, B_1, B_2 = init()
+    for i in range(iterations):
+        for j in range(len(X_trainsamples)):
+            W_1, W_2, B_1, B_2, A_2 = gradient_descent(X_trainsamples[j],Y_trainsamples[j], 1, alpha, W_1, W_2, B_1, B_2)
+            if i % 10 == 0 and j == 2:
+                a = get_accuracy(get_predictions(A_2),Y_trainsamples[j])
+                print(f"Iterations: {i}. Accuracy: {a*100}%.")
+
+
+stochastic_gradient_descent(X_trainsamples,Y_trainsamples,50000,0.5)
