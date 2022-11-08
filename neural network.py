926a08adbcc094b34f04799e8efc6d3fb7d7a929
Philippe <109043448+philippekarst@users.noreply.github.com>
2022-11-08 12:47:28 +0100

    arbitrary layers & neurons (some bugs)

--- neural network.py
+++ neural network.py
@@ -23,17 +23,19 @@ for i in range(60):
     Y_trainsamples.append(Y_train[1000*i:1000*i+1000])
 X_test = df_test[1:,:]/255
 Y_test = df_test[0,:]
-print(Y_test.shape)
 
 
-#initialize the network
+#initialize the network by specifying number of neurons in hidden layers and number of hidden layers
 def init(n :int,m : int):
-    W_1 = np.random.rand(n,784)-0.5
-    W_2 = np.random.rand(10,10)-0.5
-    B_1 = np.zeros([10,1])
-    B_2 = np.zeros([10,1])
-    return W_1, W_2, B_1, B_2
-W_1, W_2, B_1, B_2= init()
+    W = []
+    W.append(np.random.rand(n,784)-0.5)    #weights for input layer
+    for i in range(m):                     #weights for hidden layers
+        W.append(np.random.rand(n,n)-0.5)
+    B = []
+    for i in range(m):                     #weights for hidden layers
+        B.append(np.zeros([n,1]))
+    B.append(np.zeros([10,1]))             #weight for output layer
+    return W, B, m
 
 
 #define Sigmoid function
@@ -43,12 +45,13 @@ def sigmoid(x):
 
 
 #forward propagation
-def forward_prop(W_1, W_2, B_1, B_2, X):
-    Z_1 = np.dot(W_1,X)+B_1
-    A_1 = sigmoid(Z_1)
-    Z_2 = np.dot(W_2, A_1)+B_2
-    A_2 = sigmoid(Z_2)
-    return Z_1, Z_2, A_1, A_2
+def forward_prop(m, W, B, X):
+    Z=[]
+    A=[X]
+    for i in range(m+1):
+        Z.append(np.dot(W[i],A[i])+B[i])
+        A.append(sigmoid(np.dot(W[i],A[i])+B[i]))
+    return Z, A
 
 
 #onehot encode Y
@@ -60,36 +63,35 @@ def one_hot(Y):
                 one_hotY[i,j] = 1
     return one_hotY
 
-
-#back propagation
-def back_prop(Z_1, Z_2, A_1, A_2, W_2, Y, X):
+def back_prop(m, Z, A, W, Y):
     Y = one_hot(Y)
-    dW_2 = 1/(X.shape[1])*2*np.dot((A_2-Y),(sigmoid(Z_2)*(1-sigmoid(Z_2))*A_1).transpose())
-    predB_2 = 1/(X.shape[1])*np.diag(np.dot(sigmoid(Z_2)*(1-sigmoid(Z_2)),(2*(A_2-Y)).transpose()))
-    dB_2 = np.zeros([10,1])
-    for i in range(10):
-        dB_2[i,0] = predB_2[i]
-    dA_1 = 2*np.dot((A_2-Y).transpose(),W_2).transpose()
-    dW_1 = 1/(X.shape[1])*np.dot(dA_1*(sigmoid(Z_1)*(1-sigmoid(Z_1))),X.transpose())
-    predB_1 = 1/(X.shape[1])*np.diag(np.dot(sigmoid(Z_1)*(1-sigmoid(Z_1)),dA_1.transpose()))
-    dB_1 = np.zeros([10,1])
-    for i in range(10):
-        dB_1[i,0] = predB_1[i]
-    return dW_1, dB_1, dW_2, dB_2
+    #initialize list of differentials
+    dW=[]
+    dB=[]
+    dA=[2*(A[m+1]-Y)]
+    #compute differential of the activations with respect to the cost
+    for i in range(m):
+        dA.append(np.dot(dA[i].transpose(),W[m-i]).transpose()*sigmoid(Z[m-i])*(1-sigmoid(Z[m-i])))
+    #compute differentials of weights and biases with respect to the cost
+    for i in range(m+1):
+        dW.append(1/(A[0].shape[1])*np.dot((sigmoid(Z[m-i])*(1-sigmoid(Z[m-i]))*dA[0]),A[m-i].transpose()))
+        dB.append(1/(A[0].shape[1])*np.diag(np.dot(sigmoid(Z[m-i])*(1-sigmoid(Z[m-i])),(dA[0]).transpose())))
+    dW.reverse()
+    dB.reverse()
+    return dW, dB
 
 
 #update parameters
-def update_params(W_1, B_1, W_2, B_2, dW_1, dB_1, dW_2, dB_2, alpha):
-    W_1 = W_1-alpha*dW_1
-    W_2 = W_2-alpha*dW_2
-    B_1 = B_1-alpha*dB_1
-    B_2 = B_2-alpha*dB_2
-    return W_1, W_2, B_1, B_2
+def update_params(m, W, B, dW, dB, alpha):
+    for i in range(m+1):
+        W[i] = W[i]-alpha*dW[i]
+        B[i] = B[i]-alpha*dB[i]
+    return W, B
 
 
 #get the prediction
-def get_predictions(A_2):
-    return np.argmax(A_2,0)
+def get_predictions(m, A):
+    return np.argmax(A[m+1],0)
 
 
 #get accuracy
@@ -101,48 +103,47 @@ def get_accuracy(predictions, Y):
             m=m+1
     return m/(T)
 
-
 #gradient descent
-def gradient_descent(X, Y, iterations, alpha, W_1, W_2, B_1, B_2):
+def gradient_descent(m, X, Y, iterations, alpha, W, B):
     for i in range(iterations):
-        Z_1, Z_2, A_1, A_2 = forward_prop(W_1, W_2, B_1, B_2, X)
-        dW_1, dB_1, dW_2, dB_2 = back_prop(Z_1, Z_2, A_1, A_2, W_2, Y, X)
-        W_1, W_2, B_1, B_2 = update_params(W_1, B_1, W_2, B_2, dW_1, dB_1, dW_2, dB_2, alpha)
+        Z, A = forward_prop(m, W, B, X)
+        dW, dB = back_prop(m, Z, A, W, Y)
+        W, B = update_params(m, W, B, dW, dB, alpha)
         #if i % 10 == 0:
         #    a = get_accuracy(get_predictions(A_2),Y)
         #    print(f"Iterations: {i}. Accuracy: {a*100}%.")
-    return W_1, W_2, B_1, B_2, A_2
+    return W, B, A
 
 
 #stochastic gradient descent
-def stochastic_gradient_descent(X_trainsamples, Y_trainsamples, iterations, alpha):
-    W_1, W_2, B_1, B_2 = init()
+def stochastic_gradient_descent(n, m, X_trainsamples, Y_trainsamples, iterations, alpha):
+    W, B, m = init(n,m)
     for i in range(iterations):
         for j in range(len(X_trainsamples)):
-            W_1, W_2, B_1, B_2, A_2 = gradient_descent(X_trainsamples[j],Y_trainsamples[j], 1, alpha, W_1, W_2, B_1, B_2)
+            W, B, A = gradient_descent(m, X_trainsamples[j],Y_trainsamples[j], 1, alpha, W, B)
             if i % 10 == 0 and j == 0:
-                a = get_accuracy(get_predictions(A_2),Y_trainsamples[j])
+                a = get_accuracy(get_predictions(m, A),Y_trainsamples[j])
                 print(f"Iterations: {i}. Accuracy: {a*100}%.")
-        if get_accuracy(get_predictions(A_2), Y_trainsamples[59]) >= 0.9:
+        if get_accuracy(get_predictions(A[m+1]), Y_trainsamples[59]) >= 0.9:
             print("The network has finished training.")
             break
-    return W_1, W_2, B_1, B_2
+    return W, B
 
 
 #save model
-W_1, W_2, B_1, B_2 = stochastic_gradient_descent(X_trainsamples,Y_trainsamples,50000,0.5)
-print(W_1.shape)
-np.savetxt("W_1.csv",W_1,delimiter=",")
-np.savetxt("W_2.csv",W_2,delimiter=",")
-np.savetxt("B_1.csv",B_1,delimiter=",")
-np.savetxt("B_2.csv",B_2,delimiter=",")
+W, B = stochastic_gradient_descent(10, 1, X_trainsamples,Y_trainsamples,50000,0.5)
+for i in W:
+    np.savetxt(f"W_{i}.csv",W[i],delimiter=",")
+for i in B:
+    np.savetxt(f"B_{i}.csv",B[i],delimiter=",")
+
 
 #test the model
-def test_model(W_1, W_2, B_1, B_2, X, Y):
-    Z_1, Z_2, A_1, A_2 = forward_prop(W_1, W_2, B_1, B_2, X)
-    print(A_2.shape)
-    print(f"Accuracy on test data: {get_accuracy(get_predictions(A_2), Y)}")
+def test_model(m, W, B, X, Y):
+    Z, A = forward_prop(m, W, B, X)
+    print(A[m+1].shape)
+    print(f"Accuracy on test data: {get_accuracy(get_predictions(A[m+1]), Y)}")
 
 
 
-test_model(W_1, W_2, B_1, B_2, X_test, Y_test)
+#test_model(m, W, B, X_test, Y_test)
--- test_stuff.py
+++ test_stuff.py
@@ -2,5 +2,14 @@ import numpy as np
 
 a = np.array([[1,2,2],[1,1,1]])
 print(a)
-print(a.shape)
+print(a.shape[0])
 print(type(a))
+W_2 = np.random.rand(10,10)-0.5
+print(W_2)
+print(W_2.shape)
+B = []
+for i in range(1):                     #weights for hidden layers
+    B.append(np.zeros([10,1]))
+B.append(np.zeros([10,1]))
+print(B)
+print(B[0].shape)
